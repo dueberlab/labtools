908cdea2cb3d72ebb8dd016be1e29782c55687a8
znruss <zachary.n.russ@gmail.com>
2014-08-06 14:53:59 -0700

    Update renameMzdata.sh

--- renameMzdata.sh
+++ renameMzdata.sh
@@ -4,5 +4,5 @@ do
 	rm $filename
 	mv tempxml $filename
 	newname=$(grep "sampleName" $filename | sed -e 's/[a-zA-Z0-9 <]*\>\([a-zA-Z0-9 _.]*\)\<\/[a-zA-Z0-9 >]*/\1.mzdata.xml/' -e 's/ /_/' -e 's/ /_/' -e 's/ /_/' | sed -e 's/\r//g')
-	echo $filename $newname
+	mv $filename $newname
 done
