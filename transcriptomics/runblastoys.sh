30602ca697d6f1de3a39a8d88ff3b21e1a5214f2
znruss <zachary.n.russ@gmail.com>
2014-09-10 21:33:31 -0700

    Create runblastoys.sh

---
+++ transcriptomics/runblastoys.sh
@@ -0,0 +1,1 @@
+./blastoysmini.py --one ./PT1D_L.fa --two ./PT1D_R.fa --three ./PT1D_orphan.fa --four ./null.fa --null ./null.fa --seed ./seed.fa --save ./save --load ./save --print ./print.fa --temp ./
