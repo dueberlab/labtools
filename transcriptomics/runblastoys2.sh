f82578df663549b56ef9e3e4d1eeb18a1a4ac708
znruss <zachary.n.russ@gmail.com>
2014-09-28 21:32:53 -0700

    Update and rename runblastoys.sh to runblastoys2.sh

---
+++ transcriptomics/runblastoys2.sh
@@ -0,0 +1,2 @@
+./blastoys2.py --auto --maxiter 10 --inputpaths /flash/PT1D_L.fa,/flash/PT1D_R.fa,/flash/PT1D_orphan.fa --seed /flash/seed.fa --save /flash/save/01 --print /flash/print.fa --temp /flash/temp
+#./blastoys2.py --inputpaths /flash/PT1D_L.fa,/flash/PT1D_R.fa,/flash/PT1D_orphan.fa --seed /flash/seed.fa --save /flash/save/01 --print /flash/print.fa --temp /flash/temp
